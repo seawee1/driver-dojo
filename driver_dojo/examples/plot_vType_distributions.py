1457b2169f9864fe1c851daf548d49d225fbff54
seawee1 <sebastian.rietsch@pm.me>
2022-06-14 15:53:21 +0200

    Updated vType distribution plotting tool

--- driver_dojo/examples/plot_vType_distributions.py
+++ driver_dojo/examples/plot_vType_distributions.py
@@ -2,91 +2,130 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import math
 import argparse
+
+from driver_dojo.examples.utils import plot_normal, plot_uniform, plot_const
+
+
+def check_dist_string(dist):
+    import string
+    is_string = False
+    for char in string.ascii_letters:
+        if char in dist:
+            is_string = True
+
+    if 'normal' in dist:
+        return 'normal'
+    elif 'uniform' in dist:
+        return 'uniform'
+    elif is_string:
+        return 'string'
+    else:
+        return 'const'
 
 
 if __name__ == "__main__":
-    """For this to work correctly, the specifications inside the distribution file has to be groupped into blocks general/carfollow/lanechange/[
+    """For this to work correctly, the specifications inside the distribution file has to be grouped 
+    into blocks general/car-follow/lane-change/junction models, separated by an empty line.
     """
     parser = argparse.ArgumentParser(description="Plot the vType distributions.")
     parser.add_argument(
         "dist_path",
         type=str,
-        help="Path to the .txt file for createvTypeDistributions.py.",
+        help="Path to the .txt file for createVehTypeDistributions.py.",
     )
-    plt.style.use("classic")
-    sns.set()
+    parser.add_argument(
+        "--cols",
+        type=int,
+        default=6,
+        help="Number of columns to plot.",
+    )
+    parser.add_argument(
+        "--subplot_size",
+        type=int,
+        default=3,
+    )
+    args = parser.parse_args()
 
-    # fig, axes = plt.subplots(4, 4)
+    sns.set_style('white')
 
-    data = pd.read_csv("general.txt", sep=";", header=None)
-    data.columns = ["name", "dist", "clip"]
+    category_names = ["General", "Car-following Model", "Lane-change Model", "Junction Model"]
+    # Open the file
+    with open(args.dist_path) as f:
+        lines = f.read()
+        categories = []
+        for category in lines.split("\n\n"):
+            entry = category.split('\n')
+            entry = [e for e in entry if e.strip() != '']
+            categories.append(entry)
 
-    for index, row in data.iterrows():
-        name = row["name"]
-        normal = row["dist"].find("normal")
-        if normal == 1:
-            dist = row["dist"]
-            dist = dist.replace("(", "")
-            dist = dist.replace(")", "")
-            dist = dist.replace("normal", "")
-            dist_list = dist.partition(",")
-            mu = float(dist_list[0])
-            std = float(dist_list[2])
+    for c, category in enumerate(categories):
+        plot_types = [check_dist_string(entry.split(';')[1].strip()) for entry in category if entry != '']
+        num_string_plots = 0
+        for t in plot_types:
+            if t == 'string':
+                num_string_plots += 1
+        rows = math.ceil((len(category) - num_string_plots) / args.cols)
 
-            clip = row["clip"]
-            clip = clip.replace("[", "")
-            clip = clip.replace("]", "")
-            clip_list = clip.partition(",")
-            lower = float(clip_list[0])
-            upper = float(clip_list[2])
+        fig, axes = plt.subplots(rows, args.cols, figsize=(args.cols * args.subplot_size, rows * args.subplot_size))
+        # fig.suptitle(category_names[c], size='xx-large')
+        col_i = 0
+        row_i = 0
+        for entry in category:
+            if entry == '':
+                continue
+            split = entry.split(';')
+            name = split[0].strip()
+            dist = split[1].strip()
+            clip = None
 
-            s = np.random.normal(mu, std, 100000)
+            if check_dist_string(dist) == 'string':
+                print(category_names[c], name, dist)
+                continue
 
-            # for index, value in enumerate(s):
-            #     if value < lower:
-            #         s[index] = lower
-            #     elif value > upper:
-            #         s[index] = upper
+            if len(split) == 3:
+                clip = split[2].strip()
+                clip = clip.replace('[', '')
+                clip = clip.replace(']', '')
+                clip = clip.split(',')
+                clip_min = float(clip[0].strip())
+                clip_max = float(clip[1].strip())
+                clip = [clip_min, clip_max]
 
-            x = pd.Series(s, name=name)
-            ax = sns.distplot(x, hist=True)
-            plt.text(x=mu + 0.01, y=0.3, s=f"μ={mu}\nσ={std}")
-            plt.text(x=upper + 0.01, y=0.05, s=f"max={upper}")
-            plt.text(x=lower + 0.01, y=0.05, s=f"min={lower}")
+            if len(axes.shape) == 1:
+                ax = axes[col_i]
+            else:
+                ax = axes[row_i, col_i]
+            col_i = (col_i + 1) % args.cols
+            if col_i == 0:
+                row_i += 1
 
-            plt.axvline(x=upper, color="gray", ls="--", lw=2, label=f"max={upper}")
+            if check_dist_string(dist) == 'normal':
+                dist = dist[7:]
+                dist = dist.replace(")", "")
+                dist = dist.split(',')
+                mu = float(dist[0].strip())
+                std = float(dist[1].strip())
+                plot_normal(ax, name, mu, std, clip)
+            elif check_dist_string(dist) == 'uniform':
+                dist = dist[8:]
+                dist = dist.replace(")", "")
+                dist = dist.split(',')
+                mi = float(dist[0].strip())
+                ma = float(dist[1].strip())
+                plot_uniform(ax, name, mi, ma)
+            else:
+                val = float(dist.strip())
+                plot_const(ax, name, val)
 
-            plt.axvline(x=lower, color="gray", ls="--", lw=2, label=f"min={lower}")
-
-            plt.axvline(x=mu, color="gray", ls="--", lw=2, label=f"μ={mu}")
-
-            plt.axvline(x=std, color="gray", ls="--", lw=0, label=f"σ={std}")
-
-            plt.show()
-
-        uniform = row["dist"].find("uniform")
-        if uniform == 1:
-            dist = row["dist"]
-            dist = dist.replace("(", "")
-            dist = dist.replace(")", "")
-            dist = dist.replace("uniform", "")
-            dist_list = dist.partition(",")
-            low = float(dist_list[0])
-            high = float(dist_list[2])
-
-            s = np.random.uniform(low, high, 100000)
-
-            x = pd.Series(s, name=name)
-            ax = sns.distplot(x, hist=True)
-            # plt.text(x=s.mean(), y=0.3, s=f"low={low}\nhigh={high}")
-            plt.text(x=high + 0.005, y=0.1, s=f"high={high}")
-            plt.text(x=low + 0.005, y=0.1, s=f"low={low}")
-
-            plt.axvline(x=high, color="gray", ls="--", lw=2, label=f"high={high}")
-
-            plt.axvline(x=low, color="gray", ls="--", lw=2, label=f"low={low}")
-
-            plt.axvline(x=s.mean(), color="gray", ls="--", lw=2, label=f"μ={s.mean()}")
-
-            plt.show()
+        while col_i < args.cols and row_i < rows:
+            if len(axes.shape) == 1:
+                ax = axes[col_i]
+            else:
+                ax = axes[row_i, col_i]
+            fig.delaxes(ax)
+            col_i += 1
+        fig.tight_layout()
+        plt.savefig(category_names[c].lower().split(' ')[0] + '.pdf')
+        plt.show()
--- driver_dojo/examples/utils.py
+++ driver_dojo/examples/utils.py
@@ -1,4 +1,6 @@
 import yaml
+import scipy
+import numpy as np
 
 
 def read_yaml(path):
@@ -20,3 +22,90 @@ class DotDict(dict):
             raise AttributeError from e
         __setattr__ = dict.__setitem__
         __delattr__ = dict.__delitem__
+
+
+def plot_normal(ax, name, mu, std, clip):
+    dist = scipy.stats.distributions.norm
+    x = np.linspace(mu - 3 * std, mu + 3 * std, endpoint=True, num=100)
+    y = dist.pdf(x, loc=mu, scale=std)
+
+    ax.plot(x, y)
+
+    ax.axvline(x=mu,
+               color='black',
+               ls='--',
+               lw=1,
+               label='$\mathcal{N}$'f' ({mu},{std}''$^{2}$)')
+    ax.axvline(x=std,
+               color='black',
+               ls='--',
+               lw=0, )
+
+    if clip:
+        ax.axvline(x=clip[0],
+                   color='black',
+                   ls='--',
+                   lw=1,
+                   label='$x \in $'f'[{clip[0]}, {clip[1]}]')
+        ax.axvline(x=clip[1],
+                   color='black',
+                   ls='--',
+                   lw=1)
+    leg = ax.legend(handlelength=0, handletextpad=0, fancybox=True, loc='upper right')
+    for item in leg.legendHandles:
+        item.set_visible(False)
+
+    ax.set_xlim(mu - 3 * std, mu + 3 * std)
+    ax.set_ylim(0, max(y) + max(y) / 10.0)
+    ax.set_xticks([round(mu - 2 * std, 1), round(mu, 1), round(mu + 2 * std, 1)])
+    ax.set_yticks([0.0, round(max(y)/2.0, 1), round(max(y) + max(y)/10.0, 1)])
+    ax.set_aspect(1.0 / ax.get_data_ratio(), adjustable='box')
+    ax.set_title(name)
+    return ax
+
+
+def plot_uniform(ax, name, mi, ma):
+    dist = scipy.stats.distributions.uniform
+    span = ma - mi
+    x = np.linspace(mi - span / 5, ma + span / 5, endpoint=True, num=100)
+    y = dist.pdf(x, loc=mi, scale=span)
+
+    ax.plot(x, y)
+
+    ax.axvline(x=mi,
+               color='black',
+               ls='--',
+               lw=0,
+               label=f'$\mathcal{{U}}'f'_{{[{mi}, {ma}]}}''$')
+
+    leg = ax.legend(handlelength=0, handletextpad=0, fancybox=True, loc='upper right')
+    for item in leg.legendHandles:
+        item.set_visible(False)
+
+    ax.set_xlim(mi - span / 5, ma + span / 5)
+    ax.set_ylim(0, max(y) + max(y) / 10.0)
+    ax.set_xticks([round(mi, 1), round(mi + span / 2, 1), round(ma, 1)])
+    ax.set_yticks([0.0, round(max(y)/2.0, 1), round(max(y) + max(y)/10.0, 1)])
+    ax.set_aspect(1.0 / ax.get_data_ratio(), adjustable='box')
+    ax.set_title(name)
+    return ax
+
+
+def plot_const(ax, name, val):
+    ax.axvline(x=val,
+               color='black',
+               ls='-',
+               lw=1,
+               label=f"const {val}")
+
+    leg = ax.legend(handlelength=0, handletextpad=0, fancybox=True, loc='upper right')
+    for item in leg.legendHandles:
+        item.set_visible(False)
+
+    ax.set_xlim(val - 2.0, val + 2.0)
+    ax.set_ylim(0, 1.0)
+    ax.set_xticks([val])
+    ax.set_yticks([0.0, 1.0])
+    ax.set_aspect(1.0 / ax.get_data_ratio(), adjustable='box')
+    ax.set_title(name)
+    return ax
